3799ecd0e33834e7f02b97e992f2443e75acc615
Molly Lupton <molly.lupton@gmail.com>
2019-09-08 15:59:34 -0400

    heroku deployment

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,29 +3,74 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
+    <AppBar position="static" style={{background: '#90C0AC'}}>
+      <h1>The Personal Knowledge Base</h1>
       {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
+        <Toolbar>
+          <Typography variant="button">
+            <div align="right">
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home" style={{padding: 15, color: 'black'}}>
+                {' '}
+                Home
+              </Link>
+              <Link to="/mydata" style={{padding: 15, color: 'black'}}>
+                My Knowledge Base
+              </Link>
+              <a
+                href="#"
+                onClick={handleClick}
+                style={{padding: 15, color: 'black'}}
+              >
+                Logout
+              </a>
+            </div>
+          </Typography>
+        </Toolbar>
       ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
+        <Toolbar>
+          <Typography variant="button">
+            {/* The navbar will show these links before you log in */}
+            <Link to="/home" style={{padding: 15}}>
+              Home
+            </Link>
+            <Link to="/login" style={{padding: 15}}>
+              Login
+            </Link>
+            <Link to="/signup" style={{padding: 15}}>
+              Sign Up
+            </Link>
+          </Typography>
+        </Toolbar>
       )}
-    </nav>
+    </AppBar>
     <hr />
   </div>
+  //     {/* {isLoggedIn ? (
+  //       <div>
+  //         {/* The navbar will show these links after you log in */}
+  //         <Link to="/home">Home</Link>
+  //         <a href="#" onClick={handleClick}>
+  //           Logout
+  //         </a>
+  //       </div>
+  //     ) : (
+  //       <div>
+  //         {/* The navbar will show these links before you log in */}
+  //         <Link to="/login">Login</Link>
+  //         <Link to="/signup">Sign Up</Link>
+  //       </div>
+  //     )} */}
+
+  //   </nav>
+  //   <hr />
+  // </div>
 )
 
 /**
